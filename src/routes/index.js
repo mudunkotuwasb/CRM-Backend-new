fa3a7724536f3e83e138bd090dd624e982df47bd
IsharaNethmini <isharanethmini423@gmail.com>
2025-07-17 17:01:01 +0530

    refactor: Update route for company representative to use contact manager

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const authRoute = require("./auth");
 const adminRoute = require("./admin");
 const { checkRole, userAuth } = require("../controller/auth");
 const ROLES = require("../utils/roles");
-const companyRepresentativeRoute = require('./companyRepresentative');
+const contactRouter = require('./contact');
 const dataControllerRoute = require('./dataController');
 
 // Anyone can access this route
@@ -16,8 +16,8 @@ router.use("/auth", authRoute);
 // Admin Protected Route
 router.use("/admin", userAuth, checkRole([ROLES.admin]), adminRoute);
 
-// Add business protected route
-router.use("/company-representative", userAuth, checkRole([ROLES.admin, ROLES.company_representative]), companyRepresentativeRoute);
+// Admin and Company Representative protected Route
+router.use("/contact-manager", userAuth, checkRole([ROLES.admin, ROLES.company_representative]), contactRouter);
 
 // Add data controller protected route
 router.use("/dataController", userAuth, dataControllerRoute);
